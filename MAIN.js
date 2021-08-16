c38c8ac7315d63de863aff89b7540fdba8ac56fc
Lizzy Wiley <Lizzyrho@gmail.com>
2021-08-16 13:23:39 -0500

    made changes

---
+++ MAIN.js
@@ -0,0 +1,17 @@
+//=============================================== Globals ========================================================//
+let dPokeBall = document.getElementsByClassName("d-pokeball"); // grabs the element with the class name.
+let liPokeBall = document.getElementsByClassName("li-pokeball"); // grabs the element with the class name
+let specialBallArea = document.getElementsByClassName("specialBall"); // grabs the element with the class name.
+let contactUsArea = document.getElementById('contact-us'); // grabs the element with the class name.
+
+//===================================== Things we may need ==================================// 
+// we will need a data saving functions and a constructor. We can go class method
+// we will need 
+
+//===================================== Demarcus ========================================//
+function confirmPageChange() {
+
+
+
+
+}
